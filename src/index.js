f05633a8f93c111597fa41345bb3d5d54915755e
FranciscoMaik <chiconunes3081@gmail.com>
2020-06-30 18:19:07 -0300

    Verificaçao JWT

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const db = require("./models");
 const response = require("./middlewares/response");
+const checkJwt = require("./middlewares/jwt");
 const authController = require("./controllers/auth");
 const linkController = require("./controllers/link");
 
 const app = express();
 
 app.use(response);
+app.use(checkJwt);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
